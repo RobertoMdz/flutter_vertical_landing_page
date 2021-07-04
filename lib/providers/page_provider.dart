aa0fdb5e02fb754128033d7991682308b92ddc52
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-07-04 18:51:53 -0500

    move to page when enter the url or when the user slide de page

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController pageController = PageController();
+  List<String> pages = ['home', 'about', 'pricing', 'contact', 'location'];
+  int _currentIndex = 0;
 
-  goToPage(int pageIndex) {
+  void createScrollController(String routeName) {
+    pageController = PageController(initialPage: getPageIndex(routeName));
+
+    pageController.addListener(() {
+      final pageIndex = (pageController.page ?? 0).round();
+
+      if (pageIndex != _currentIndex) {
+        html.window.history.pushState(null, 'none', '#/${pages[pageIndex]}');
+        _currentIndex = pageIndex;
+      }
+    });
+  }
+
+  int getPageIndex(String routeName) {
+    final pageIndex = pages.indexOf(routeName);
+    if (pageIndex != -1) {
+      return pageIndex;
+    }
+    return 0;
+  }
+
+  void goToPage(int pageIndex) {
     pageController.animateToPage(
       pageIndex,
       duration: Duration(milliseconds: 200),
--- lib/router/router_handlers.dart
+++ lib/router/router_handlers.dart
@@ -1,10 +1,16 @@
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
+import 'package:vertical_landing_page/providers/page_provider.dart';
 
 import '../ui/pages/home_page.dart';
 
 final homeHandler = Handler(
   handlerFunc: (context, params) {
     final pageRoute = params['page']!.first;
-    if (pageRoute != '/') return HomePage();
+    if (pageRoute != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(pageRoute);
+      return HomePage();
+    }
   },
 );
